c7bf5e793d8f9ab7bf1a4da94ea8444d5f99ca3e
Vincent Bui <vincent.bui657@gmail.com>
2024-03-30 23:58:48 -0400

    Added functions for word generation and game completion

--- app.js
+++ app.js
@@ -19,3 +19,20 @@ const resetGame = () => {
   keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
   gameModal.classList.remove("show");
 }
+
+const getRandomWord = () => {
+  // Selecting a random word and hint from the wordList
+  const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+  currentWord = word; // Making currentWord as random word
+  document.querySelector(".hint-text b").innerText = hint;
+  resetGame();
+}
+
+const gameOver = (isVictory) => {
+  // After game complete.. showing modal with relevant details
+  const modalText = isVictory ? `You found the word:` : 'The correct word was:';
+  gameModal.querySelector("img").src = `images/${isVictory ? 'victory' : 'lost'}.gif`;
+  gameModal.querySelector("h4").innerText = isVictory ? 'Congrats!' : 'Game Over!';
+  gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
+  gameModal.classList.add("show");
+}
