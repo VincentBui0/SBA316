48f33439ebd7700e7aeab5a81d33af195d0684b4
Vincent Bui <vincent.bui657@gmail.com>
2024-03-31 12:06:23 -0400

    Make game keyboard to make game playable

--- app.js
+++ app.js
@@ -60,4 +60,13 @@ const initGame = (button, clickedLetter) => {
     if(wrongGuessCount === maxGuesses) return gameOver(false);
     if(correctLetters.length === currentWord.length) return gameOver(true);
 }
+// Creating keyboard buttons and adding event listeners
+for (let i = 97; i <= 122; i++) {
+  const button = document.createElement("button");
+  button.innerText = String.fromCharCode(i);
+  keyboardDiv.appendChild(button);
+  button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
+}
 
+getRandomWord();
+playAgainBtn.addEventListener("click", getRandomWord);
